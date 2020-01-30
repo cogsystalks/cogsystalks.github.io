010290e10eeb20dfb1cefc92de71a88d386b5ec8
Federico Bergamin <bergamin.federico@gmail.com>
2020-01-30 17:25:40 +0100

    Fix form, trial n3

--- php/form_to_email.php
+++ php/form_to_email.php
@@ -1,31 +1,38 @@
 <?php
 if(isset($_POST['submit'])){
-    $to = "cogsystalks@gmail.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $first_name = $_POST['name'];
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = $first_name .  " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+  if(empty($_POST['name'])  ||
+  empty($_POST['email']) ||
+  empty($_POST['message']))
+  {
+    $errors .= "\n Error: all fields are required";
+  }
 
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
+  $to = "cogsystalks@gmail.com"; // this is your Email address
+  $from = $_POST['email']; // this is the sender's Email address
+  $first_name = $_POST['name'];
+  $subject = "Form submission";
+  $subject2 = "Copy of your form submission";
+  $message = $first_name .  " wrote the following:" . "\n\n" . $_POST['message'];
+  $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+  $headers = "From:" . $from;
+  $headers2 = "From:" . $to;
 
 
-    response = mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    $response = mail($to_email,$subject,$message,$headers);
+  response = mail($to,$subject,$message,$headers);
+  mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+  $response = mail($to_email,$subject,$message,$headers);
 
 
-    if($response)
-    {
-      echo "Thank you for showing an interest on this project," . $first_name . ". \n We will
-      contact you shortly.";
-    }
-    else
-    {
-      echo "Sorry for the inconvenience" . $first_name . ". Try to send the email again.";
-    }
+  if($response)
+  {
+    echo "Thank you for showing an interest on this project," . $first_name . ". \n We will
+    contact you shortly.";
+  }
+  else
+  {
+    echo "Sorry for the inconvenience" . $first_name . ". Try to send the email again.";
+  }
 
-   }
+}
 ?>
